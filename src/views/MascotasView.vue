c77d4bac1b1f7f51d29ddf0f2c457cb0dff72955
ADRIAN LOPEZ <adrianinsuasty@udenar.edu.co>
2022-10-17 11:05:10 -0500

    Add method get pet

--- src/views/MascotasView.vue
+++ src/views/MascotasView.vue
@@ -1,14 +1,40 @@
 <template>
-  <v-container> Lista de mascotas </v-container>
+  <v-container>
+    <MascotaList :mascotas="mascotas" />
+  </v-container>
 </template>
 
 <script>
 import { defineComponent } from "vue";
 
+import MascotaList from "@/components/Mascotas/MascotaList";
+import axios from "axios";
+
 export default defineComponent({
   // eslint-disable-next-line vue/multi-word-component-names
   name: "Mascotas",
 
-  components: {},
+  data: () => ({
+    mascotas: [],
+  }),
+
+  components: { MascotaList },
+
+  methods: {
+    getPets() {
+      axios
+        .get("http://localhost:5000/mascotas")
+        .then((response) => {
+          this.mascotas = response?.data?.rows ?? [];
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+
+  created() {
+    this.getPets();
+  },
 });
 </script>
